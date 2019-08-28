4ceb988da48eee74afc4c2351e1b3d05792abf17
Alexandre Arnold <alexandre.arnold@airbus.com>
2019-08-28 15:41:53 +0200

    Add hub concept (to replace and extend catalog & wrappers)

--- python/airlaps/__init__.py
+++ python/airlaps/__init__.py
@@ -1,3 +1,5 @@
 from airlaps.core import *
 from airlaps.domains import *
 from airlaps.solvers import *
+from airlaps.utils import *
+from airlaps import hub
--- python/setup.py
+++ python/setup.py
@@ -2,27 +2,13 @@ from setuptools import setup
 
 setup(
     name='airlaps',
-    version='0.1.0',
+    version='0.2.0',
     packages=[
         'airlaps',
-        'airlaps.catalog',
-        'airlaps.catalog.domain',
-        'airlaps.catalog.solver',
         'airlaps.builders',
         'airlaps.builders.domain',
-        'airlaps.builders.solver',
-        'airlaps.wrappers',
-        'airlaps.wrappers.space',
-        'airlaps.wrappers.domain',
-        'airlaps.wrappers.solver',
-        ],
-    extras_require={
-        'wrappers': [
-            'gym==0.13.0',
-            'stable-baselines==2.6.0',
-            'tensorflow==1.14.0'
-        ],
-    },
+        'airlaps.builders.solver'
+    ],
     url='www.airbus.com',
     license='MIT',
     author='Airbus',
