add9407f65ef9c928cf71a0be6d596f3f50cbc36
gdupont <ger.dupont@gmail.com>
2019-09-17 17:25:13 +0200

    identifying the dependency to simplejson - only version 3.16.0 has been tested

--- python/setup.py
+++ python/setup.py
@@ -13,6 +13,9 @@ setup(
         'airlaps.builders.domain',
         'airlaps.builders.solver'
     ],
+    install_requires=[
+       'simplejson>=3.16.0'
+    ],
     url='www.airbus.com',
     license='MIT',
     author='Airbus',
