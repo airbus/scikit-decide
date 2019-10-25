cf7b5e99d608ad6432bfd6ed8beb23be577bf4cd
Florent Teichteil-koenigsbuch <florent.teichteil-koenigsbuch@airbus.com>
2019-10-25 19:46:15 +0200

    Draw path on live map for gym jsbsim envs

--- python/examples/gym_jsbsim_envs.py
+++ python/examples/gym_jsbsim_envs.py
@@ -9,8 +9,11 @@
 import gym
 import gym_jsbsim
 import numpy as np
+import folium
+import os
+import json
 
-from typing import Callable
+from typing import Callable, Any
 
 from airlaps import TransitionOutcome, TransitionValue
 from airlaps.hub.domain.gym import DeterministicInitializedGymDomain, GymWidthDomain, GymDiscreteActionDomain
@@ -23,6 +26,8 @@ from gym_jsbsim.catalogs.catalog import Catalog as prp
 # ENV_NAME = 'GymJsbsim-HeadingControlTask-v0'
 ENV_NAME = 'GymJsbsim-TaxiapControlTask-v0'
 HORIZON = 200
+
+PATH_FILE = os.path.join(os.path.join(os.path.dirname(gym_jsbsim.__file__), 'docs'), 'points.json')
 
 gym_env = gym.make(ENV_NAME)
 gym_env._max_episode_steps = HORIZON
@@ -62,11 +67,37 @@ class GymRIWDomain(D):
                                          discretization_factor=discretization_factor,
                                          branching_factor=branching_factor)
         GymWidthDomain.__init__(self)
+        self._map = None
+        self._current_point = None
     
     def _state_step(self, action: D.T_agent[D.T_concurrency[D.T_event]]) -> TransitionOutcome[
             D.T_state, D.T_agent[TransitionValue[D.T_value]], D.T_agent[D.T_info]]:
         o = super()._state_step(action)
         return TransitionOutcome(state=o.state, value=TransitionValue(reward=-o.value.reward), termination=o.termination, info=o.info)
+    
+    def _render_from(self, memory: D.T_memory[D.T_state], **kwargs: Any) -> Any:
+        # Get rid of the current state and just look at the gym env's current intneral state
+        lon = self._gym_env.sim.get_property_value(prp.position_long_gc_deg)
+        lat = self._gym_env.sim.get_property_value(prp.position_lat_geod_deg)
+        alt = self._gym_env.sim.get_property_value(prp.position_h_sl_ft)
+        psi = self._gym_env.sim.get_property_value(prp.attitude_psi_rad)
+        theta = self._gym_env.sim.get_property_value(prp.attitude_theta_rad)
+        phi = self._gym_env.sim.get_property_value(prp.attitude_phi_rad)
+        if (self._map is None):
+            self._map = folium.Map(location=[lat, lon], zoom_start=18)
+            self._map.get_root().header.add_child(folium.Element('<script type="text/javascript" src="http://livejs.com/live.js"></script>'))
+            self._current_point = (lat, lon)
+            with open(PATH_FILE) as json_file:
+                data = json.load(json_file)
+                points = []
+                for p, c in data.items():
+                    folium.Marker((c[1], c[0]), popup=p).add_to(self._map)
+                    points.append((c[1], c[0]))
+                folium.PolyLine(points, color='blue', weight=2.5, opacity=1).add_to(self._map)
+        else:
+            folium.PolyLine([self._current_point, (lat, lon)], color='red', weight=2.5, opacity=1).add_to(self._map)
+            self._current_point = (lat, lon)
+        self._map.save('gym_jsbsim_map.html')
 
 
 domain_factory = lambda: GymRIWDomain(gym_env=gym_env,
@@ -97,67 +128,3 @@ if RIW.check_domain(domain):
     solver = GymRIWDomain.solve_with(solver_factory, domain_factory)
     initial_state = solver._domain.reset()
     rollout(domain, solver, from_memory=initial_state, num_episodes=1, max_steps=HORIZON, max_framerate=30, outcome_formatter=None, action_formatter=None)
-
-    # init_state = domain.get_initial_state()
-    # solver.solve_from(init_state)
-    # plan = []
-    # current_state = init_state
-
-    # while True:
-    #     print('current state:', str(current_state))
-    #     if solver._is_solution_defined_for(current_state):
-    #         plan.append(solver.get_next_action(current_state))
-    #         current_state = domain.get_next_state(current_state, plan[-1])
-    #         # lon = domain._gym_env.sim.get_property_value(prp.position_long_gc_deg)
-    #         # lat = domain._gym_env.sim.get_property_value(prp.position_lat_geod_deg)
-    #         # print(str(lon), str(lat))
-    #     else:
-    #         break
-    # print('PLAN:', str(plan))
-
-    # traj_string = []
-    # attitude_string = []
-
-    # while not done:
-    #     ...
-    #     lon = self.env.sim.get_property_value(prp.position_long_gc_deg)
-    #     lat = self.env.sim.get_property_value(prp.position_lat_geod_deg)
-    #     alt = self.env.sim.get_property_value(prp.position_h_sl_ft)
-    #     psi = self.env.sim.get_property_value(prp.attitude_psi_rad)
-    #     theta = self.env.sim.get_property_value(prp.attitude_theta_rad)
-    #     phi = self.env.sim.get_property_value(prp.attitude_phi_rad)
-
-    #     sim_time = self.env.sim.get_property_value(prp.simulation_sim_time_sec)
-
-    #     traj_string += [sim_time, lon, lat, alt]
-    #     attitude_string += [sim_time, psi, theta, phi]
-
-    #     print("\n==========\nTRAJECTOIRE\n==========\n")
-    #     for t in range(0, len(traj_string) - 5, +4):
-    #         print(str(traj_string[t]) + "," + str(traj_string[t + 1]) + "," + str(traj_string[t + 2]) + "," + str(traj_string[t + 3]) + ",")
-    #     print("\n==========\nATTITUDE\n==========\n")
-    #     for t in range(0, len(attitude_string) - 5, +4):
-    #         print(str(attitude_string[t]) + "," + str(attitude_string[t + 1]) + "," + str(attitude_string[t + 2]) + "," + str(attitude_string[t + 3]) + ",")
-
-
-    #         f = open('cart3D_taxi_CGP.json', 'w')
-    #         f.write('cart3D_taxi_CGP=[')
-    #         for t in range(0, len(traj_string) - 5, +4):
-    #             f.write(str(traj_string[t]) + "," + str(traj_string[t + 1]) + "," + str(traj_string[t + 2]) + "," + str(traj_string[t + 3]))
-    #             if t == len(traj_string) - 8:
-    #                 f.write(']')
-    #             else:
-    #                 f.write(',')
-    #         f.close()
-
-
-    #         f = open('angles_taxi_CGP.json', 'w')
-    #         f.write('angles_taxi_CGP=[')
-    #         for t in range(0, len(attitude_string) - 5, +4):
-    #             f.write(str(attitude_string[t]) + "," + str(attitude_string[t + 1]) + "," + str(
-    #                 attitude_string[t + 2]) + "," + str(attitude_string[t + 3]))
-    #             if t == len(traj_string) - 8:
-    #                 f.write(']')
-    #             else:
-    #                 f.write(',')
-    #         f.close()
