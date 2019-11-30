8ee154cb42b4769df73b84a044a48d2fa86caccb
Florent Teichteil-koenigsbuch <florent.teichteil-koenigsbuch@airbus.com>
2019-11-30 19:49:21 +0100

    Progresssing on WRL

--- python/examples/wrl_gym_solver.py
+++ python/examples/wrl_gym_solver.py
@@ -17,7 +17,7 @@ import numpy as np
 from typing import Callable, Any
 
 from airlaps.core import EnvironmentOutcome, TransitionValue
-from airlaps.hub.domain.gym import GymDomain, GymDomainHashable, GymWidthDomain
+from airlaps.hub.domain.gym import GymDomain, GymWidthDomain
 from airlaps.hub.solver.stable_baselines import StableBaseline
 from airlaps.hub.solver.wrl import WidthEnvironmentDomain
 from airlaps.utils import rollout
@@ -25,39 +25,17 @@ from airlaps.utils import rollout
 ENV_NAME = 'MountainCarContinuous-v0'
 HORIZON = 200
 
-class D(GymDomainHashable, GymWidthDomain):
+class D(GymDomain, GymWidthDomain):
     pass
 
 class GymWRLDomain(D):
     def __init__(self, gym_env: gym.Env,
                        continuous_feature_fidelity: int = 1):
-        GymDomainHashable.__init__(self, gym_env=gym_env)
+        GymDomain.__init__(self, gym_env=gym_env)
         GymWidthDomain.__init__(self, continuous_feature_fidelity=continuous_feature_fidelity)
 
 
-class MyWidthEnvironmentDomain(WidthEnvironmentDomain):
-    def __init__(self, domain: GymWRLDomain,
-                        state_features: Callable[[D.T_observation, GymWRLDomain], Any],
-                        initial_pruning_probability: float = 0.999,
-                        temperature_increase_rate: float = 0.01,
-                        width_increase_resilience: int = 10,
-                        max_depth: int = 1000,
-                        use_state_feature_hash: bool = False,
-                        cache_transitions: bool = False,
-                        debug_logs: bool = False) -> None:
-        super().__init__(domain, state_features, initial_pruning_probability,
-                         temperature_increase_rate, width_increase_resilience,
-                         max_depth, use_state_feature_hash, cache_transitions, debug_logs)
-    
-    def reset(self) -> D.T_agent[D.T_observation]:
-        return super().reset()._state
-    
-    def step(self, action: D.T_agent[D.T_concurrency[D.T_event]]) -> EnvironmentOutcome[
-                D.T_agent[D.T_observation], D.T_agent[TransitionValue[D.T_value]], D.T_agent[D.T_info]]:
-        return super().step(action)._state
-
-
-proxy_domain_factory = lambda: MyWidthEnvironmentDomain(domain=GymWRLDomain(gym.make(ENV_NAME)),
+proxy_domain_factory = lambda: WidthEnvironmentDomain(domain=GymWRLDomain(gym.make(ENV_NAME)),
                                                       state_features=lambda s, d: d.bee_features(s),
                                                       initial_pruning_probability=0.999,
                                                       temperature_increase_rate=0.001,
@@ -65,7 +43,7 @@ proxy_domain_factory = lambda: MyWidthEnvironmentDomain(domain=GymWRLDomain(gym.
                                                       max_depth=HORIZON,
                                                       use_state_feature_hash=False,
                                                       cache_transitions=True,
-                                                      debug_logs=True)
+                                                      debug_logs=False)
 proxy_domain = proxy_domain_factory()
 
 domain_factory = lambda: GymDomain(gym.make(ENV_NAME))
