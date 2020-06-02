f519bfe474d80b181be7f0e464ea71671201a368
SCHNEIDER <schneider@TOLA302007494.local>
2020-06-02 17:19:11 +0200

    improve CGP gym domain test

--- skdecide/hub/solver/cgp/cgp.py
+++ skdecide/hub/solver/cgp/cgp.py
@@ -183,13 +183,8 @@ class CGPWrapper(Solver, DeterministicPolicies, Restorable):
         """
         CGP manage all kind of gym types, BOX, DISCRETE and TUPLE as well
         """
-        return isinstance(domain.get_action_space().unwrapped(), gym.spaces.Tuple)
-                and isinstance(domain.get_observation_space.unwrapped(), gym.spaces.Tuple)
-                and isinstance(domain.get_action_space().unwrapped(), gym.spaces.Discrete)
-                and isinstance(domain.get_observation_space.unwrapped(), gym.spaces.Discrete)
-                and isinstance(domain.get_action_space().unwrapped(), gym.spaces.Box)
-                and isinstance(domain.get_observation_space.unwrapped(), gym.spaces.Box)
-
+        return isinstance(domain.get_action_space().unwrapped(), (gym.spaces.Tuple, gym.spaces.Discrete, gym.spaces.Box))
+                and isinstance(domain.get_observation_space.unwrapped(), (gym.spaces.Tuple, gym.spaces.Discrete, gym.spaces.Box))
 
     def _solve_domain(self, domain_factory: Callable[[], D]) -> None:
         domain = domain_factory()
