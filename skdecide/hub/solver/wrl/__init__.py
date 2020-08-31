1fa03906526c708610b70b7abe2dc01fe01870a7
Florent Teichteil-koenigsbuch <florent.teichteil-koenigsbuch@airbus.com>
2020-08-31 11:02:08 +0200

    Removing solver WRL

--- skdecide/hub/solver/wrl/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) AIRBUS and its affiliates.
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-from .wrl import WidthEnvironmentDomain, WidthSimulationDomain, WidthMetaSolver
