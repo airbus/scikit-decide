176639d6a9eaa0644fd08de0c5a0c29a7a61f59c
Guillaume Alleon <guillaume.alleon@gmail.com>
2021-10-07 18:53:54 +0200

    Implement multi version documentation
    - generate a static web site on GH pages for each tagged release
      and for master (dev version)
    - create vuejs component to choose selected version
    - modify default theme to add version component in navbar
    - move all vuepress related packages as dev dependencies
    - config.js used instead of config.yml

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+    lang: 'en-US',
+    title: 'Scikit-decide',
+    description: 'This is scikit-decide documentation site',
+    base: `/scikit-decide${process.env.DOCS_VERSION_PATH || '/'}`,
+
+    locales: {
+        '/': {
+            lang: 'en-US',
+            title: 'Scikit-decide',
+            description: 'This is scikit-decide documentation',
+        },
+    },
+
+    themeConfig: {
+        repo: 'airbus/scikit-decide',
+        logo: '/logo.svg',
+        editLinks: false,
+        docsDir: '',
+        editLinkText: '',
+        lastUpdated: false,
+        locales: {
+            '/': {
+                selectLanguageName: 'en-US',
+            },
+        },
+        nav: [
+            {
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'Guide',
+                link: '/guide/'
+            },
+            {
+                text: 'Reference',
+                link: '/reference/'
+            },
+        ],
+        sidebar: 'auto',
+    },
+
+    plugins: {
+        '@vuepress/plugin-back-to-top': {},
+        'mathjax': {
+            target: 'svg',
+            macros: { '*': '\\times' }
+        },
+    },
+}
