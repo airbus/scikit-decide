6a486684722dfbae0b31eb48b13d7c87593774bb
POVEDA_G <guillaume.poveda@gmail.com>
2021-10-26 17:44:12 +0200

    Add an example on scheduling domains
    This can be used to highlight difference of computation time between
    - a domain using deepcopy and
    - one doing inplace modification of the objects

---
+++ examples/scheduling/computation_time_experiments.py
@@ -0,0 +1,91 @@
+import time
+
+from examples.discrete_optimization.rcpsp_multiskill_parser_example import (
+    get_complete_path_ms,
+    get_data_available_ms,
+)
+from examples.discrete_optimization.rcpsp_parser_example import (
+    get_complete_path,
+    get_data_available,
+)
+from skdecide import DiscreteDistribution, rollout_episode
+from skdecide.builders.domain.scheduling.scheduling_domains_modelling import (
+    SchedulingAction,
+    SchedulingActionEnum,
+    State,
+)
+from skdecide.hub.domain.rcpsp.rcpsp_sk import MSRCPSP, RCPSP
+from skdecide.hub.domain.rcpsp.rcpsp_sk_parser import (
+    load_domain,
+    load_multiskill_domain,
+)
+from skdecide.hub.solver.do_solver.do_solver_scheduling import DOSolver, SolvingMethod
+from skdecide.hub.solver.sgs_policies.sgs_policies import (
+    BasePolicyMethod,
+    PolicyMethodParams,
+)
+
+
+def do_rollout_comparaison(domain: RCPSP, solver, inplace: bool = True):
+    domain.set_inplace_environment(inplace)
+    tic = time.perf_counter()
+    states, actions, values = rollout_episode(
+        domain=domain,
+        solver=solver,
+        from_memory=domain.get_initial_state(),
+        max_steps=1000,
+        outcome_formatter=None,
+        action_formatter=None,
+        verbose=False,
+    )
+    toc = time.perf_counter()
+    print(
+        f"{toc - tic:0.4f} seconds to rollout policy with inplace={inplace} environment, "
+        f"Final time of the schedule : "
+        f"{states[-1].t}"
+    )
+    return states
+
+
+def run_expe():
+    do_solver = SolvingMethod.PILE  # Greedy solver.
+    domain: RCPSP = load_domain(get_complete_path("j1201_1.sm"))
+    solver = DOSolver(
+        policy_method_params=PolicyMethodParams(
+            base_policy_method=BasePolicyMethod.FOLLOW_GANTT,
+            # policy will just follow the output gantt of the greedy solver
+            delta_index_freedom=0,
+            delta_time_freedom=0,
+        ),
+        method=do_solver,
+    )
+    solver.solve(domain_factory=lambda: domain)
+    states_deepcopy = do_rollout_comparaison(domain, solver, False)
+    states_inplace = do_rollout_comparaison(domain, solver, True)
+
+    # main difference is that in the inplace environment, the object are overwritten as you can see
+    print([id(s) for s in states_inplace])
+    # Where as the deepcopy solution is creating each time a new object :
+    print([id(s) for s in states_deepcopy])
+
+    # There is a 20 factor speedup, and it's even more when the makespan is large, let's try :
+    domain: RCPSP = load_domain(get_complete_path("j1201_1.sm"))
+    for task in domain.duration_dict:
+        for mode in domain.duration_dict[task]:
+            domain.duration_dict[task][mode] *= 20
+    domain.set_inplace_environment(False)
+    solver = DOSolver(
+        policy_method_params=PolicyMethodParams(
+            base_policy_method=BasePolicyMethod.FOLLOW_GANTT,
+            delta_index_freedom=0,
+            delta_time_freedom=0,
+        ),
+        method=do_solver,
+    )
+    solver.solve(domain_factory=lambda: domain)
+    states_deepcopy = do_rollout_comparaison(domain, solver, False)
+    states_inplace = do_rollout_comparaison(domain, solver, True)
+
+
+if __name__ == "__main__":
+    run_expe()
