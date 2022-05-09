8943a60fe463dfcd422d5087273e0e21dd5ee991
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2022-05-09 14:21:29 +0200

    Remove previous embedded discrete_optimization code
    Indeed this is included in the stand-alone library
    discrete-optimization

--- skdecide/discrete_optimization/generic_tools/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) AIRBUS and its affiliates.
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
--- skdecide/discrete_optimization/rcpsp/solver/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) AIRBUS and its affiliates.
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-from .cp_solvers import CP_MRCPSP_MZN, CP_RCPSP_MZN
-from .rcpsp_lp_solver import LP_MRCPSP, LP_RCPSP, LP_RCPSP_Solver
-from .rcpsp_pile import PileSolverRCPSP, PileSolverRCPSP_Calendar
