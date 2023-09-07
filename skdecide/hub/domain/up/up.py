f6dc61b4685c3c876a2cb800e0aab81a2414a8c3
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2023-09-07 18:53:03 +0200

    Final UP bridge

--- skdecide/hub/domain/up/up.py
+++ skdecide/hub/domain/up/up.py
@@ -2,45 +2,197 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from unified_planning.shortcuts import Problem, ProblemKind
+from typing import List, Optional, Tuple, Union
 
-from skdecide import Domain, ImplicitSpace, Space, TransitionOutcome, Value
-from skdecide.builders.domain import (
-    DeterministicInitialized,
-    DeterministicTransitions,
-    FullyObservable,
-    Goals,
-    Initializable,
-    Markovian,
-    Memoryless,
-    PositiveCosts,
-    Sequential,
-    SingleAgent,
-    UnrestrictedActions,
+import unified_planning as up
+from unified_planning.model.state import UPState
+
+from skdecide.core import ImplicitSpace, Space, Value
+from skdecide.domains import DeterministicPlanningDomain
+from skdecide.hub.space.gym import ListSpace, MultiBinarySpace
+from skdecide.utils import logger
+
+from unified_planning.model import UPState, InstantaneousAction, Problem
+from unified_planning.plans import ActionInstance
+from unified_planning.model.metrics import (
+    MaximizeExpressionOnFinalState,
+    Oversubscription,
+    TemporalOversubscription,
 )
-from skdecide.hub.space.gym import GymSpace, ListSpace
-
-class D(
-    Domain,
-    SingleAgent,
-    Sequential,
-    UnrestrictedActions,
-    Initializable,
-    Memoryless,
-    FullyObservable,
-    PositiveCosts,
-):
-    pass
+from unified_planning.shortcuts import FluentExp
+from unified_planning.engines.sequential_simulator import (
+    UPSequentialSimulator,
+    evaluate_quality_metric,
+)
+from unified_planning.engines.compilers.grounder import GrounderHelper
+from unified_planning.exceptions import UPValueError
 
 
-class UPDomainFactory:
-    """This class wraps a Unified Planning's problem (up.model.Problem) as a scikit-decide domain.
-    Depending on the problem kind (up.model.ProblemKind) of the UP problem, this class will create
-    a domain with specific features that corresond to the actual ProblemKind
+class SkUPState:
+    def __init__(self, up_state: UPState):
+        self._up_state = up_state
+
+    @property
+    def up_state(self):
+        return self._up_state
+
+    def __hash__(self):
+        return hash(frozenset(self._up_state._values.items()))
+
+    def __eq__(self, other):
+        return self._up_state._values == other._up_state._values
+
+    def __repr__(self) -> str:
+        return repr(self._up_state)
+
+    def __str__(self) -> str:
+        return str(self._up_state)
+
+
+class SkUPAction:
+    def __init__(self, up_action: Union[InstantaneousAction, ActionInstance]):
+        if not isinstance(up_action, (InstantaneousAction, ActionInstance)):
+            raise RuntimeError(
+                f"SkUPAction: action {up_action} must be an instance of either InstantaneousAction or ActionInstance"
+            )
+        self._up_action = up_action
+
+    @property
+    def up_action(self) -> InstantaneousAction:
+        return (
+            self._up_action
+            if isinstance(self._up_action, InstantaneousAction)
+            else self._up_action.action
+        )
+
+    @property
+    def up_parameters(
+        self,
+    ) -> Union[List[up.model.parameter.Parameter], Tuple[up.model.FNode, ...]]:
+        return (
+            self._up_action.parameters
+            if isinstance(self._up_action, InstantaneousAction)
+            else self._up_action.actual_parameters
+        )
+
+    def __hash__(self):
+        return hash(self.up_action)
+
+    def __eq__(self, other):
+        return self.up_action == other.up_action
+
+    def __repr__(self) -> str:
+        return repr(self._up_action)
+
+    def __str__(self) -> str:
+        return str(self._up_action)
+
+
+class D(DeterministicPlanningDomain):
+    T_state = SkUPState  # Type of states
+    T_observation = T_state  # Type of observations
+    T_event = SkUPAction  # Type of events
+    T_value = float  # Type of transition values (rewards or costs)
+    T_info = None  # Type of additional information in environment outcome
+
+
+class UPDomain(D):
+    """This class wraps Unified Planning problem as a scikit-decide domain.
 
     !!! warning
-        Using this class requires unified-planning to be installed.
+        Using this class requires unified_planning to be installed.
     """
-    
-    def __init__(self, up_problem: Problem) -> None:
-        pass
+
+    def __init__(self, problem: Problem, **params):
+        """Initialize UPDomain.
+
+        # Parameters
+        problem: The Unified Planning problem (Problem) to wrap.
+        params: Optional parameters to pass to the UP sequential simulator
+        """
+        self._problem = problem
+        self._simulator = UPSequentialSimulator(
+            problem, error_on_failed_checks=True, **params
+        )
+        try:
+            self._total_cost = FluentExp(problem.fluent("total-cost"))
+        except UPValueError:
+            self._total_cost = None
+        self._transition_costs = {}
+
+    def _get_next_state(self, memory: D.T_state, action: D.T_event) -> D.T_state:
+        if self._total_cost is not None:
+            cost = memory.up_state.get_value(self._total_cost)
+        state = SkUPState(self._simulator.apply(memory.up_state, action._up_action))
+        if self._total_cost is not None:
+            cost = state.up_state.get_value(self._total_cost) - cost
+            self._transition_costs[tuple([memory, action, state])] = cost
+        return state
+
+    def _get_transition_value(
+        self,
+        memory: D.T_state,
+        action: D.T_event,
+        next_state: Optional[D.T_state] = None,
+    ) -> Value[D.T_value]:
+        transition = tuple([memory, action, next_state])
+        if self._total_cost is not None and transition in self._transition_costs:
+            return Value(cost=self._transition_costs[transition])
+        elif len(self._problem.quality_metrics) > 0:
+            transition_cost = 0
+            for qm in self._problem.quality_metrics:
+                metric = evaluate_quality_metric(
+                    self._simulator,
+                    qm,
+                    0,
+                    memory.up_state,
+                    action.up_action,
+                    action.up_parameters,
+                    next_state.up_state,
+                )
+                if isinstance(
+                    qm,
+                    (
+                        MaximizeExpressionOnFinalState,
+                        Oversubscription,
+                        TemporalOversubscription,
+                    ),
+                ):
+                    transition_cost += -metric
+                else:
+                    transition_cost += metric
+            return Value(cost=transition_cost)
+        else:
+            logger.warning(
+                "UPDomain: requesting transition value whereas the 'total-cost' fluent or UP quality metrics are not defined will return NaN"
+            )
+            return Value(cost=float("Nan"))
+
+    def _is_terminal(self, memory: D.T_state) -> D.T_predicate:
+        return self._simulator.is_goal(memory.up_state)
+
+    def _get_action_space_(self) -> Space[D.T_event]:
+        return ListSpace(
+            [
+                SkUPAction(a)
+                for a in GrounderHelper(self._problem).get_grounded_actions()
+            ]
+        )
+
+    def _get_applicable_actions_from(self, memory: D.T_state) -> Space[D.T_event]:
+        app_actions = []
+        for action, params in self._simulator.get_applicable_actions(memory.up_state):
+            app_actions.append(
+                SkUPAction(self._simulator._ground_action(action, params))
+            )
+        return ListSpace(app_actions)
+
+    def _get_goals_(self) -> Space[D.T_observation]:
+        return ImplicitSpace(lambda s: self._is_terminal(s))
+
+    def _get_initial_state_(self) -> D.T_state:
+        return SkUPState(self._simulator.get_initial_state())
+
+    def _get_observation_space_(self) -> Space[D.T_observation]:
+        # TODO: not clear what to do here, it will depend on the algorithm
+        return MultiBinarySpace(len(self._initial_state_dict))
---
+++ skdecide/hub/solver/up/up.py
@@ -0,0 +1,86 @@
+# Copyright (c) AIRBUS and its affiliates.
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+from unified_planning.exceptions import UPValueError
+from unified_planning.shortcuts import FluentExp, SequentialSimulator
+
+from skdecide.hub.domain.up import UPDomain, SkUPState, SkUPAction
+from typing import Callable, List
+
+from skdecide import Solver, Value
+from skdecide.builders.solver import DeterministicPolicies, Utilities
+
+
+# TODO: remove Markovian req?
+class D(UPDomain):
+    pass
+
+
+class UPSolver(Solver, DeterministicPolicies, Utilities):
+    """This class wraps a Unified Planning engine as a scikit-decide solver.
+
+    !!! warning
+        Using this class requires unified-planning[engine] to be installed.
+    """
+
+    T_domain = D
+
+    def __init__(self, operation_mode, **params) -> None:
+        """Initialize UPSolver.
+
+        # Parameters
+        operation_mode: UP operation mode class.
+        params: The optional parameters to pass to the constructor of the UP operation mode object.
+        """
+        super().__init__()
+        self._operation_mode = operation_mode
+        self._params = params
+        self._plan = []
+        self._policy = {}
+        self._values = {}
+
+    def _solve_domain(self, domain_factory: Callable[[], D]) -> None:
+        self._domain = domain_factory()
+        problem = self._domain._problem
+        params = (
+            self._params if len(self._params) > 0 else {"problem_kind": problem.kind}
+        )
+        with self._operation_mode(**params) as planner:
+            result = planner.solve(problem)
+            self._plan = [SkUPAction(a) for a in result.plan.actions]
+            plan_extractor_domain = domain_factory()
+            state = plan_extractor_domain.get_initial_state()
+            self._values[state] = Value(cost=0)
+            plan_cost = Value(cost=0)
+            state_sequence = [state]
+            for ai in self._plan:
+                self._policy[state] = ai
+                next_state = plan_extractor_domain.get_next_state(state, ai)
+                transition_cost = plan_extractor_domain.get_transition_value(
+                    state, ai, next_state
+                )
+                plan_cost = Value(cost=plan_cost.cost + transition_cost.cost)
+                self._values[next_state] = plan_cost
+                state_sequence.append(next_state)
+                state = next_state
+            for state in state_sequence:
+                self._values[state] = Value(
+                    cost=plan_cost.cost - self._values[state].cost
+                )
+
+    def _get_next_action(
+        self, observation: D.T_agent[D.T_observation]
+    ) -> D.T_agent[D.T_concurrency[D.T_event]]:
+        return self._policy[observation]
+
+    def _is_policy_defined_for(self, observation: D.T_agent[D.T_observation]) -> bool:
+        return observation in self._policy
+
+    def _get_utility(self, observation: D.T_agent[D.T_observation]) -> D.T_value:
+        return self._values[observation]
+
+    def get_plan(self) -> List[SkUPAction]:
+        return self._plan
