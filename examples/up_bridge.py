86082d7abd7face3e6240f3bcec7ee2b7594c8c0
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-10-09 15:07:40 +0200

    Add --all-files option to precommit command to avoid skipping the test
    No tests were actually done by the linter action because `pre-commit
    run` look only files in the index by default.
    When checkout a whole PR, no files are in the index so no files are
    checked.
    
    Previously, the job returned something like
    
    	Trim Trailing Whitespace.............................(no files to check)Skipped
    	Fix End of Files.....................................(no files to check)Skipped
    	Check Yaml...........................................(no files to check)Skipped
        	Check for added large files..........................(no files to check)Skipped
        	Pretty format JSON...................................(no files to check)Skipped
        	isort (python).......................................(no files to check)Skipped
        	black................................................(no files to check)Skipped
        	nbqa-isort...........................................(no files to check)Skipped
        	nbqa-black...........................................(no files to check)Skipped
        	nbqa-pycln...........................................(no files to check)Skipped
        	nbstripout...........................................(no files to check)Skipped
    
    Now the checks are done (and should read as "Passed")
    We make a `pre-commit run --all-files` in this commit to have the job
    pass.
    
    We also remove the pretty-format-json hook which was tampering with the
    notebooks (oddly only remotely on the github runner, not locally on my
    laptop), and was not very useful for the repository anyway.

--- examples/up_bridge.py
+++ examples/up_bridge.py
@@ -3,20 +3,20 @@
 # LICENSE file in the root directory of this source tree.
 
 import sys
-from skdecide.hub.domain.up import UPDomain
-from skdecide.hub.solver.up import UPSolver
-from skdecide.hub.solver.lazy_astar import LazyAstar
 
 import unified_planning
 from unified_planning.shortcuts import (
-    UserType,
     BoolType,
-    OneshotPlanner,
     Fluent,
     InstantaneousAction,
     Not,
+    OneshotPlanner,
+    UserType,
 )
 
+from skdecide.hub.domain.up import UPDomain
+from skdecide.hub.solver.lazy_astar import LazyAstar
+from skdecide.hub.solver.up import UPSolver
 from skdecide.utils import rollout
 
 # Example 1: Solving a basic example, the same as
