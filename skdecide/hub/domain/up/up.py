f25450978601e46a073ac4ef556d9aab73c189e5
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2023-11-17 00:12:13 +0100

    Add dict and vec state encoding in UPDomain

--- skdecide/hub/domain/up/up.py
+++ skdecide/hub/domain/up/up.py
@@ -3,6 +3,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from typing import Dict, List, Optional, Tuple, Union
+import numpy as np
 import gym
 
 import unified_planning as up
@@ -10,7 +11,7 @@ from unified_planning.model.state import UPState
 
 from skdecide.core import ImplicitSpace, Space, Value
 from skdecide.domains import DeterministicPlanningDomain
-from skdecide.hub.space.gym import ListSpace, DictSpace
+from skdecide.hub.space.gym import ListSpace, DictSpace, BoxSpace
 from skdecide.utils import logger
 
 from unified_planning.model import Problem, UPState, InstantaneousAction, FNode
@@ -38,10 +39,20 @@ class SkUPState:
         return self._up_state
 
     def __hash__(self):
-        return hash(frozenset(self._up_state._values.items()))
+        return hash(
+            frozenset(
+                (fn, v)
+                for fn, v in self._up_state._values.items()
+                if fn.fluent().name != "total-cost"
+            )
+        )
 
     def __eq__(self, other):
-        return self._up_state._values == other._up_state._values
+        return {
+            fn: v for fn, v in self._up_state if fn.fluent().name != "total-cost"
+        }._values == {
+            fn: v for fn, v in other._up_state if fn.fluent().name != "total-cost"
+        }._values
 
     def __repr__(self) -> str:
         return repr(self._up_state)
@@ -51,17 +62,26 @@ class SkUPState:
 
 
 class SkUPAction:
-    def __init__(self, up_action: Union[InstantaneousAction, ActionInstance]):
+    def __init__(
+        self,
+        up_action: Union[InstantaneousAction, ActionInstance],
+        ungrounded_action: InstantaneousAction = None,
+        orig_params: Tuple[FNode, ...] = None,
+    ):
         if not isinstance(up_action, (InstantaneousAction, ActionInstance)):
             raise RuntimeError(
                 f"SkUPAction: action {up_action} must be an instance of either InstantaneousAction or ActionInstance"
             )
         self._up_action = up_action
+        self._ungrounded_action = ungrounded_action
+        self._orig_params = orig_params
 
     @property
     def up_action(self) -> InstantaneousAction:
         return (
-            self._up_action
+            self._ungrounded_action
+            if self._ungrounded_action is not None
+            else self._up_action
             if isinstance(self._up_action, InstantaneousAction)
             else self._up_action.action
         )
@@ -71,7 +91,9 @@ class SkUPAction:
         self,
     ) -> Union[List[up.model.parameter.Parameter], Tuple[up.model.FNode, ...]]:
         return (
-            self._up_action.parameters
+            self._orig_params
+            if self._orig_params is not None
+            else self._up_action.parameters
             if isinstance(self._up_action, InstantaneousAction)
             else self._up_action.actual_parameters
         )
@@ -101,7 +123,7 @@ class SkUPAction:
 
 
 class D(DeterministicPlanningDomain):
-    T_state = SkUPState  # Type of states
+    T_state = Union[SkUPState, DictSpace, BoxSpace]  # Type of states
     T_observation = T_state  # Type of observations
     T_event = SkUPAction  # Type of events
     T_value = float  # Type of transition values (rewards or costs)
@@ -118,8 +140,8 @@ class UPDomain(D):
     def __init__(
         self,
         problem: Problem,
-        int_fluent_domains: Dict[FNode, int] = None,
-        real_fluent_domains: Dict[FNode, Tuple[float, float]] = None,
+        fluent_domains: Dict[FNode, Tuple[Union[int, float], Union[int, float]]] = None,
+        state_encoding: str = "native",
         **simulator_params,
     ):
         """Initialize UPDomain.
@@ -139,17 +161,143 @@ class UPDomain(D):
         except UPValueError:
             self._total_cost = None
         self._transition_costs = {}
-        self._int_fluent_domains = int_fluent_domains
-        self._real_fluent_domains = real_fluent_domains
+        self._fluent_domains = fluent_domains
+        self._state_encoding = state_encoding
+        self._fnodes_variables_map = None
+        self._fnodes_vars_ordering = None
+        self._states_map = None
+        if self._state_encoding != "native":
+            if self._state_encoding not in ["dictionary", "vector"]:
+                raise RuntimeError(
+                    "State encoding must be 'native', 'dictionary' or 'vector'"
+                )
+            self._init_state_encoding_()
+
+    def _init_state_encoding_(self):
+        def fnode_lower_bound(fn):
+            if fn.fluent().type.lower_bound is not None:
+                return fn.fluent().type.lower_bound
+            elif self._fluent_domains is not None and fn in self._fluent_domains:
+                return self._fluent_domains[fn][0]
+            else:
+                raise RuntimeError(
+                    f"Lower bound not provided for fluent expression {fn}"
+                )
+
+        def fnode_upper_bound(fn):
+            if fn.fluent().type.upper_bound is not None:
+                return fn.fluent().type.upper_bound
+            elif self._fluent_domains is not None and fn in self._fluent_domains:
+                return self._fluent_domains[fn][1]
+            else:
+                raise RuntimeError(
+                    f"Upper bound not provided for fluent expression {fn}"
+                )
+
+        self._fnodes_variables_map = {}
+        self._fnodes_vars_ordering = []
+        self._states_map = {}
+        init_state = self._simulator.get_initial_state()
+        static_fluents = self._problem.get_static_fluents()
+        for fn in init_state._values.keys():
+            if fn.fluent() not in static_fluents and fn.fluent().name != "total-cost":
+                self._fnodes_vars_ordering.append(fn)
+                if fn.fluent().type.is_bool_type():
+                    self._fnodes_variables_map[fn] = (
+                        0,
+                        1,
+                        lambda b: int(b),
+                        lambda i: bool(i),
+                    )
+                elif fn.fluent().type.is_int_type():
+                    lb = int(fnode_lower_bound(fn))
+                    ub = int(fnode_upper_bound(fn))
+                    self._fnodes_variables_map[fn] = (
+                        (
+                            0,
+                            ub - lb + 1,
+                            lambda i, lb=lb: i - lb,
+                            lambda i, lb=lb: i + lb,
+                        )
+                        if self._state_encoding == "dictionary"
+                        else (lb, ub, lambda i: int(i), lambda i: i)
+                    )
+                elif fn.fluent().type.is_real_type():
+                    self._fnodes_variables_map[fn] = (
+                        float(fnode_lower_bound(fn)),
+                        float(fnode_upper_bound(fn)),
+                        lambda x: float(x),
+                        lambda x: x,
+                    )
+                elif fn.fluent().type.is_user_type():
+                    co = list(self._problem.objects(fn.fluent().type))
+                    o2i = {o: i for i, o in enumerate(co)}
+                    self._fnodes_variables_map[fn] = (
+                        0,
+                        len(co),
+                        lambda o, o2i=o2i: o2i[o],
+                        lambda i, i2o=co: i2o[i],
+                    )
+                elif fn.fluent().type.is_time_type():
+                    raise RuntimeError("Time types not handled by UPDomain")
+
+    def _convert_to_skup_state_(self, state):
+        if self._state_encoding == "native":
+            return state
+        elif self._state_encoding == "dictionary":
+            return self._states_map[frozenset(state.items())]
+        elif self._state_encoding == "vector":
+            return self._states_map[tuple(state.flatten())]
+        else:
+            return None
+
+    def _convert_from_skup_state_(self, skup_state: SkUPState):
+        if self._state_encoding == "native":
+            return skup_state
+        elif self._state_encoding == "dictionary":
+            state = {}
+            for fn, val in skup_state._up_state._values.items():
+                if fn in self._fnodes_variables_map:
+                    v = (
+                        val.object()
+                        if fn.fluent().type.is_user_type()
+                        else val.constant_value()
+                    )
+                    state[fn] = self._fnodes_variables_map[fn][2](v)
+            self._states_map[frozenset(state.items())] = skup_state
+            return state
+        elif self._state_encoding == "vector":
+            state = []
+            any_real = False
+            for fn in self._fnodes_vars_ordering:
+                v = (
+                    skup_state._up_state._values[fn].object()
+                    if fn.fluent().type.is_user_type()
+                    else skup_state._up_state._values[fn].constant_value()
+                )
+                state.append(self._fnodes_variables_map[fn][2](v))
+                any_real = any_real or fn.fluent().type.is_real_type()
+            state = np.array(state, dtype=np.float32 if any_real else np.int32)
+            self._states_map[tuple(state.flatten())] = skup_state
+            return state
+        else:
+            return None
 
     def _get_next_state(self, memory: D.T_state, action: D.T_event) -> D.T_state:
+        state = self._convert_to_skup_state_(memory)
         if self._total_cost is not None:
-            cost = memory.up_state.get_value(self._total_cost)
-        state = SkUPState(self._simulator.apply(memory.up_state, action._up_action))
+            cost = state.up_state.get_value(self._total_cost).constant_value()
+        next_state = SkUPState(
+            self._simulator.apply(
+                state.up_state, action.up_action, action.up_parameters
+            )
+        )
         if self._total_cost is not None:
-            cost = state.up_state.get_value(self._total_cost) - cost
-            self._transition_costs[tuple([memory, action, state])] = cost
-        return state
+            cost = (
+                next_state.up_state.get_value(self._total_cost).constant_value() - cost
+            )
+            self._transition_costs[tuple([state, action, next_state])] = cost
+        return self._convert_from_skup_state_(next_state)
 
     def _get_transition_value(
         self,
@@ -157,10 +305,17 @@ class UPDomain(D):
         action: D.T_event,
         next_state: Optional[D.T_state] = None,
     ) -> Value[D.T_value]:
-        transition = tuple([memory, action, next_state])
-        if self._total_cost is not None and transition in self._transition_costs:
-            return Value(cost=self._transition_costs[transition])
-        elif len(self._problem.quality_metrics) > 0:
+        if self._total_cost is not None:
+            transition = tuple(
+                [
+                    self._convert_to_skup_state_(memory),
+                    action,
+                    self._convert_to_skup_state_(next_state),
+                ]
+            )
+            if transition in self._transition_costs:
+                return Value(cost=self._transition_costs[transition])
+        if len(self._problem.quality_metrics) > 0:
             transition_cost = 0
             for qm in self._problem.quality_metrics:
                 metric = evaluate_quality_metric(
@@ -196,7 +351,7 @@ class UPDomain(D):
     def _get_action_space_(self) -> Space[D.T_event]:
         return ListSpace(
             [
-                SkUPAction(a[2])
+                SkUPAction(a[2], ungrounded_action=a[0], orig_params=a[1])
                 for a in GrounderHelper(self._problem).get_grounded_actions()
                 if a[2] is not None
             ]
@@ -205,7 +360,11 @@ class UPDomain(D):
     def _get_applicable_actions_from(self, memory: D.T_state) -> Space[D.T_event]:
         return ListSpace(
             [
-                SkUPAction(self._simulator._ground_action(action, params))
+                SkUPAction(
+                    self._simulator._ground_action(action, params),
+                    ungrounded_action=action,
+                    orig_params=params,
+                )
                 for action, params in self._simulator.get_applicable_actions(
                     memory.up_state
                 )
@@ -216,18 +375,49 @@ class UPDomain(D):
         return ImplicitSpace(lambda s: self._is_terminal(s))
 
     def _get_initial_state_(self) -> D.T_state:
-        return SkUPState(self._simulator.get_initial_state())
+        return self._convert_from_skup_state_(
+            SkUPState(self._simulator.get_initial_state())
+        )
 
     def _get_observation_space_(self) -> Space[D.T_observation]:
-        return DictSpace(
-            {
-                repr(k): gym.spaces.Discrete(2)
-                if v.is_bool_constant()
-                else gym.spaces.Discrete(self._int_fluent_domains[v])
-                if v.is_int_constant()
-                else gym.spaces.Box(
-                    self._real_fluent_domains[v][0], self._real_fluent_domains[v][1]
+        if self._state_encoding == "native":
+            raise RuntimeError(
+                "Observation space defined only for state encoding 'dictionary' or 'vector'"
+            )
+        elif self._state_encoding == "dictionary":
+            return DictSpace(
+                {
+                    repr(fn): gym.spaces.Discrete(2)
+                    if fn.fluent().type.is_bool_type()
+                    else gym.spaces.Discrete(v[1])
+                    if fn.fluent().type.is_int_type()
+                    else gym.spaces.Box(v[0], v[1])
+                    if fn.fluent().type.is_real_type()
+                    else gym.spaces.Discrete(v[1])
+                    if fn.fluent().type.is_user_type()
+                    else None
+                    for fn, v in self._fnodes_variables_map.items()
+                }
+            )
+        elif self._state_encoding == "vector":
+            return BoxSpace(
+                low=np.array(
+                    [
+                        self._fnodes_variables_map[fn][0]
+                        for fn in self._fnodes_vars_ordering
+                    ]
+                ),
+                high=np.array(
+                    [
+                        self._fnodes_variables_map[fn][1]
+                        for fn in self._fnodes_vars_ordering
+                    ]
+                ),
+                dtype=np.float32
+                if any(
+                    fn.fluent().type.is_real_type() for fn in self._fnodes_vars_ordering
                 )
-                for k, v in self._simulator.get_initial_state()._values.items()
-            }
-        )
+                else np.int32,
+            )
+        else:
+            return None
