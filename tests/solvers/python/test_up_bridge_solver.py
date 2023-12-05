a8a2919e37f5dad5fe5ea7fc7f91745a77a0093e
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-12-05 18:17:09 +0100

    Skip test_up_bridge_solver_numeric on macos
    This is temporary because of a crash during UP solve()

--- tests/solvers/python/test_up_bridge_solver.py
+++ tests/solvers/python/test_up_bridge_solver.py
@@ -80,8 +80,8 @@ def test_up_bridge_solver_classic():
 
 
 @pytest.mark.skipif(
-    sys.version_info < (3, 10) or platform.system() == "Windows",
-    reason="requires python3.10 or higher and any OS different from Windows",
+    sys.version_info < (3, 10) or platform.system() in ("Windows", "Darwin"),
+    reason="requires python3.10 or higher and any OS different from Windows or MacOS",
 )
 def test_up_bridge_solver_numeric():
     noexcept = True
