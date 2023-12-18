2b76d0e204baa3d5dd376451be1a9ce2916d750e
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2023-12-18 15:00:53 +0100

    Refactor flight planning domain
    - Remove "hard" import of hub solvers (Astar and LazyAstar) in this hub domain
    - Fix fuel loop optionaly used to compute loaded fuel:
      - make single_fuel_loop a function outside of the class
      - add a an optional solver_factory to use with it (instead of forcing
        LazyAstar). Use LazyAstar by default.
      - add an option to decide the tolerance on fuel used to stop the
        optimization
    - Remove custom solve from FlightDomain which was performing a solve + a
      custom rollout. Keep only the custom rollout in `self.custom_rollout()`.
    - Simplify the flight planning test and add another one testing the fuel
      loop
    - Add a docstring to the flight domain class based on the README.md
      which was put in the subpackage flight_domain (but remained hidden)
    - Remove unreachable README.md

--- tests/flight_planning/test_flight_planning.py
+++ tests/flight_planning/test_flight_planning.py
@@ -1,6 +1,7 @@
 import sys
 
 import pytest
+from openap.prop import aircraft
 
 from skdecide.hub.solver.lazy_astar import LazyAstar
 
@@ -11,34 +12,58 @@ from skdecide.hub.solver.lazy_astar import LazyAstar
 @pytest.mark.skipif(
     sys.platform.startswith("win"), reason="pygrib does not install on windows"
 )
-@pytest.mark.xfail(
-    reason="Fails mysteriously to import the c++ library on github runners."
-)
 def test_flight_planning():
     from skdecide.hub.domain.flight_planning.domain import FlightPlanningDomain
 
-    noexcept = True
+    origin = "LFPG"
+    destination = "LFBO"
+    actype = "A320"
+    heuristic = "lazy_fuel"
+    cost_function = "fuel"
+    domain_factory = lambda: FlightPlanningDomain(
+        origin=origin,
+        destination=destination,
+        actype=actype,
+        heuristic_name=heuristic,
+        objective=cost_function,
+        fuel_loop=False,
+        graph_width="normal",
+    )
+    domain = domain_factory()
+    solver = LazyAstar(heuristic=lambda d, s: d.heuristic(s))
+    domain.solve_with(solver=solver, domain_factory=domain_factory)
 
-    try:
-        origin = "LFPG"
-        destination = "LFBO"
-        aircraft = "A320"
-        heuristic = "lazy_fuel"
-        cost_function = "fuel"
-        domain_factory = lambda: FlightPlanningDomain(
-            origin,
-            destination,
-            aircraft,
-            heuristic_name=heuristic,
-            objective=cost_function,
-            fuel_loop=False,
-            graph_width="normal",
-        )
-        domain = domain_factory()
-        solver = LazyAstar(heuristic=lambda d, s: d.heuristic(s))
-        domain.solve(domain_factory, make_img=False, solver=solver)
+    assert solver.check_domain(domain)
 
-    except Exception as e:
-        print(e)
-        noexcept = False
-    assert solver.check_domain(domain) and noexcept
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 9), reason="cartopy requires python3.9 or higher"
+)
+@pytest.mark.skipif(
+    sys.platform.startswith("win"), reason="pygrib does not install on windows"
+)
+def test_flight_planning_fuel_loop():
+    from skdecide.hub.domain.flight_planning.domain import FlightPlanningDomain
+
+    origin = "LFPG"
+    destination = "LFBO"
+    actype = "A320"
+    heuristic = "lazy_fuel"
+    cost_function = "fuel"
+    constraints = {"fuel": aircraft(actype)["limits"]["MFC"]}
+    domain_factory = lambda: FlightPlanningDomain(
+        origin=origin,
+        destination=destination,
+        actype=actype,
+        constraints=constraints,
+        heuristic_name=heuristic,
+        objective=cost_function,
+        fuel_loop=True,
+        graph_width="normal",
+        fuel_loop_tol=1.0,
+    )
+    domain = domain_factory()
+    solver = LazyAstar(heuristic=lambda d, s: d.heuristic(s))
+    domain.solve_with(solver=solver, domain_factory=domain_factory)
+
+    assert solver.check_domain(domain)
