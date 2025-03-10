0f379c8fef92494d1a057b5036791be6ea1fefcb
nhuet <nolwen.huet@imacs.polytechnique.fr>
2025-03-10 15:36:14 +0100

    Monkey patch directly rllib concat_samples() function (#475)
    During training of gnn's with rllib, concat_samples was actually called
    several times with samples of graphs with potentially different
    structure. So it is "easier" to directly monkey patch it (even though
    monkey patching a function is a bit more tedious than patching a
    method).

---
+++ skdecide/hub/solver/ray_rllib/gnn/policy/sample_batch_original_code.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from ray.rllib.utils.typing import SampleBatchType
+
+
+def original_concat_samples(samples: list[SampleBatchType]) -> SampleBatchType:
+    """Function placeholder to be used to store the original concat_samples code"""
+    ...
--- skdecide/hub/solver/ray_rllib/gnn/utils/monkey_patch.py
+++ skdecide/hub/solver/ray_rllib/gnn/utils/monkey_patch.py
@@ -2,17 +2,17 @@ from skdecide.hub.solver.ray_rllib.gnn.evaluation.collectors.agent_collector imp
     monkey_patch_agent_collector,
     unmonkey_patch_agent_collector,
 )
-from skdecide.hub.solver.ray_rllib.gnn.evaluation.collectors.simple_list_collector import (
-    monkey_patch_policy_collector,
-    unmonkey_patch_policy_collector,
+from skdecide.hub.solver.ray_rllib.gnn.policy.sample_batch_monkey_patch import (
+    monkey_patch_concat_samples,
+    unmonkey_patch_concat_samples,
 )
 
 
 def monkey_patch_rllib_for_graph(graph2node: bool = False):
     monkey_patch_agent_collector(graph2node=graph2node)
-    monkey_patch_policy_collector(graph2node=graph2node)
+    monkey_patch_concat_samples(graph2node=graph2node)
 
 
 def unmonkey_patch_rllib_for_graph():
     unmonkey_patch_agent_collector()
-    unmonkey_patch_policy_collector()
+    unmonkey_patch_concat_samples()
