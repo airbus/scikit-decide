334576bfda0da124ab01f868dcfa4ede5f374f8e
Nolwen <nolwen.huet@imacs.polytechnique.fr>
2025-07-07 15:16:50 +0200

    Allow to load a autoregressive graph2node sb3 algo with different action space
    When ome action components are graph nodes the algo can apply on a
    different action space. Still
    - len(action_space.nvec) ie nb of action components cannot change
      (as the model has a network by component)
    - independent components must keep the same dim (as it is hard coded in
      the network in that case)
    
    This can be useful when training a autoregressive GNN model on a PDDL
    domain and then apply on a similar pddl domain but different problem
    (with same actions but different objects)
    
    To achieve that we just need to be careful to update the algo
    action_space. The parameters of the torch layers are still loaded as
    before.

--- skdecide/hub/solver/stable_baselines/autoregressive/ppo/autoregressive_ppo.py
+++ skdecide/hub/solver/stable_baselines/autoregressive/ppo/autoregressive_ppo.py
@@ -1,7 +1,14 @@
-from typing import ClassVar
+import io
+import pathlib
+from typing import Any, ClassVar, Dict, Optional, Type, Union
 
+import gymnasium as gym
+import torch as th
 from sb3_contrib import MaskablePPO
+from stable_baselines3.common.base_class import SelfBaseAlgorithm
 from stable_baselines3.common.policies import BasePolicy
+from stable_baselines3.common.save_util import load_from_zip_file, save_to_zip_file
+from stable_baselines3.common.type_aliases import GymEnv
 
 from skdecide.hub.solver.stable_baselines.autoregressive.common.on_policy_algorithm import (
     ApplicableActionsGraphOnPolicyAlgorithm,
@@ -54,3 +61,79 @@ class AutoregressiveGraphPPO(
         "Graph2NodePolicy": AutoregressiveGraph2NodeActorCriticPolicy,
         "HeteroGraph2NodePolicy": AutoregressiveHeteroGraph2NodeActorCriticPolicy,
     }
+
+    @classmethod
+    def load(  # noqa: C901
+        cls: Type[SelfBaseAlgorithm],
+        path: Union[str, pathlib.Path, io.BufferedIOBase],
+        env: Optional[GymEnv] = None,
+        device: Union[th.device, str] = "auto",
+        custom_objects: Optional[Dict[str, Any]] = None,
+        print_system_info: bool = False,
+        force_reset: bool = True,
+        **kwargs,
+    ) -> SelfBaseAlgorithm:
+        try:
+            return super().load(
+                path=path,
+                env=env,
+                device=device,
+                custom_objects=custom_objects,
+                print_system_info=print_system_info,
+                force_reset=force_reset,
+                **kwargs,
+            )
+        except ValueError as e:
+            # maybe error because different actions spaces (ex: apply on same pddl domain, different problem)
+            # can be ok if same number of components and dim differ only for graph2node components,
+            # but we need to update the action space
+            data, params, pytorch_variables = load_from_zip_file(path)  # stored params
+            if issubclass(
+                data["policy_class"], AutoregressiveGraph2NodeActorCriticPolicy
+            ):
+                if (
+                    "policy_kwargs" not in data
+                    or "n_graph2node_components" not in data["policy_kwargs"]
+                ):
+                    n_graph2node_components = data[
+                        "policy_class"
+                    ].default_n_graph2node_components(data["action_space"])
+                else:
+                    n_graph2node_components = data["policy_kwargs"][
+                        "n_graph2node_components"
+                    ]
+                # check action spaces have still same length
+                assert isinstance(env.action_space, gym.spaces.MultiDiscrete)
+                assert len(env.action_space.nvec) == len(
+                    data["action_space"].nvec
+                ), f"Action spaces must have same length: len({env.action_space}.nvec) != len({data['action_space']}.nvec)"
+                # check independent from graph component have same dim
+                for i_component in range(
+                    len(env.action_space.nvec) - n_graph2node_components
+                ):
+                    assert (
+                        env.action_space.nvec[i_component]
+                        == data["action_space"].nvec[i_component]
+                    ), f"Action spaces independent components must have same dim: comp #{i_component} of {env.action_space} and {data['action_space']}"
+
+                # update action space
+                data["action_space"] = env.action_space
+                # create in-memory storage with updated data and retry loading from it
+                with io.BytesIO() as file:
+                    save_to_zip_file(
+                        file,
+                        data=data,
+                        params=params,
+                        pytorch_variables=pytorch_variables,
+                    )
+                    return super().load(
+                        path=file,
+                        env=env,
+                        device=device,
+                        custom_objects=custom_objects,
+                        print_system_info=print_system_info,
+                        force_reset=force_reset,
+                        **kwargs,
+                    )
+            # else still error
+            raise e
